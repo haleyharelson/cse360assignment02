d1ef821fd17f6eabd13f86e30aa9b2628360d263
Haley Harelson <hharelso@asu.edu>
2020-10-05 21:18:29 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,58 @@
+package cse360assignment02;
+
+/**
+ * This class represents an arithmetic calculator that performs add,
+ * subtract, getTotal, and clear methods.
+ */
+
+public class AddingMachine {
+
+    /**
+     * Instance variable total to represent the total amount
+     */
+    private int total;
+
+    /**
+     * Default constructor
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * Method to get current total
+     * @return total
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * Method to add value to total
+     * @param value
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * Method to subtract value from total
+     * @param value
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * Method to return history of the adding machine's computations
+     * @return history
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * Method to "clear" the adding machine's memory
+     */
+    public void clear() {
+    }
+
+}
